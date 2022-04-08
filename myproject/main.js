6d34085080082ed85fb23acc0ef3228bf5e465e5
KyrahWiggins <kyrah.wiggins@gmail.com>
2022-04-08 11:35:23 -0400

    update

--- myproject/main.js
+++ myproject/main.js
@@ -21,11 +21,11 @@ renderer.render(scene, camera);
 
 // Torus
 
-const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
-const material = new THREE.MeshStandardMaterial({ color: 'pink' });
-const torus = new THREE.Mesh(geometry, material);
+const geometry = new THREE.TorusGeometry( 10, 2, 16, 70 );
+const material = new THREE.MeshBasicMaterial( { color: 'pink' } );
+const torus = new THREE.Mesh( geometry, material );
 
-scene.add(torus);
+scene.add( torus );
 
 // Lights
 
